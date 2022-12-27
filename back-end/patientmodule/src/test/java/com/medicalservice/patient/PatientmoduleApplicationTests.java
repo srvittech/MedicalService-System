aaa52514e7b1b103380d1a984e19f6c8da3e23c5
Komali.Indurthi <Komali.Indurthi@thryvedigital.com>
2022-12-27 15:45:59 +0530

    test

---
+++ back-end/patientmodule/src/test/java/com/medicalservice/patient/PatientmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalservice.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
