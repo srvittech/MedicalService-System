e2bb4cf2edd7474c5d28d43365b52d6a5e63d990
Sourav Kumar <srvkrak@gmail.com>
2022-12-27 17:15:56 +0530

    doctormodulecomplted

--- back-end/doctormodule/src/main/java/com/medicalservice/doctor/service/DoctorService.java
+++ back-end/doctormodule/src/main/java/com/medicalservice/doctor/service/DoctorService.java
@@ -7,7 +7,7 @@ import com.medicalservice.doctor.model.Doctor;
 public interface DoctorService {
 	public List<Doctor> getDoctors();
 	public void  addDoctor(Doctor doctor);
-	public void updateDoctor(Doctor doctor);
+//	public void updateDoctor(Doctor doctor);
 	public Doctor findDoctorById(int id);
 	public void deleteDoctorById(int id);
 
--- back-end/doctormodule/src/main/java/com/medicalservice/doctor/service/DoctorServiceImp.java
+++ back-end/doctormodule/src/main/java/com/medicalservice/doctor/service/DoctorServiceImp.java
@@ -2,37 +2,45 @@ package com.medicalservice.doctor.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.medicalservice.doctor.model.Doctor;
-
+import com.medicalservice.doctor.repository.DoctorRepository;
+@Service
 public class DoctorServiceImp implements DoctorService{
-
+	@Autowired
+	DoctorRepository doctorRepo;
+	
 	@Override
 	public List<Doctor> getDoctors() {
-		// TODO Auto-generated method stub
-		return null;
+	
+		return doctorRepo.findAll();
 	}
 
 	@Override
 	public void addDoctor(Doctor doctor) {
-		// TODO Auto-generated method stub
-		
+
+		doctorRepo.save(doctor);
 	}
 
-	@Override
-	public void updateDoctor(Doctor doctor) {
-		// TODO Auto-generated method stub
-		
-	}
+//	@Override
+//	public void updateDoctor(Doctor doctor) {
+//	
+//		
+//	}
 
 	@Override
 	public Doctor findDoctorById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		return doctorRepo.findById(id).orElse(null);
 	}
 
 	@Override
 	public void deleteDoctorById(int id) {
-		// TODO Auto-generated method stub
+	
+		doctorRepo.deleteById(id);
+		
 		
 	}
 
