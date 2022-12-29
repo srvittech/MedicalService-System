0a3216f74c17089e85abb4e16c5c139c65968fed
LakshmanReddy.Vemireddy <LakshmanReddy.Vemireddy@thryvedigital.com>
2022-12-29 22:05:21 +0530

    okay

--- back-end/feedbackmodule/src/main/java/com/medicalservice/feedback/repository/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicalservice.feedback.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.medicalservice.feedback.model.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback,Integer>{
-
-}
