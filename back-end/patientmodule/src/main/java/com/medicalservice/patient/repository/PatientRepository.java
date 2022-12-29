59a14415f9cd46e74a185e5defab93c01a5ff87b
LakshmanReddy.Vemireddy <LakshmanReddy.Vemireddy@thryvedigital.com>
2022-12-29 12:52:57 +0530

    cmt

--- back-end/patientmodule/src/main/java/com/medicalservice/patient/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicalservice.patient.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.medicalservice.patient.model.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient,Long> {
-
-}
