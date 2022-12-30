8c846290e3ded1988afea77f84d708ef6adb59c8
Sourav Kumar <srvkrak@gmail.com>
2022-12-30 22:41:58 +0530

    okay

--- back-end/back-end/doctormodule/src/main/java/com/medicalservice/doctor/repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicalservice.doctor.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.medicalservice.doctor.model.Doctor;
-
-public interface DoctorRepository extends MongoRepository<Doctor, Long> {
-
-}
--- back-end/back-end/usermodule/src/main/java/com/medicalservice/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicalservice.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.medicalservice.user.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
