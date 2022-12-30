f1ccf6427c8d918c3d6fd22e838a67647817c52a
pavanimuttineni <pavanim296@gmail.com>
2022-12-30 22:26:31 +0530

    pavani

--- back-end/usermodule/src/main/java/com/medicalservice/user/controller/UserController.java
+++ back-end/usermodule/src/main/java/com/medicalservice/user/controller/UserController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.medicalservice.user.model.User;
@@ -18,5 +21,14 @@ public class UserController {
 	public List<User> getLogin() {
 		return userServiceImpl.getLogin();
 	}
+	@PostMapping("/addLogin")
+	public User addLogin(@RequestBody User user) {
+		return userServiceImpl.addLogin(user);
+	}
+	@PutMapping("/updateLogin/{id}")
+	public User updateLogin(@RequestBody User user) {
+		return userServiceImpl.updateLogin(user);
+	}
+	}
 
-}
+
