367a8005a2b41b23a157c356795c4e8a155ea2c8
LakshmanReddy.Vemireddy <LakshmanReddy.Vemireddy@thryvedigital.com>
2022-12-31 13:39:02 +0530

    add

--- back-end/patientModuleMS/src/test/java/com/medicalservicesystem/patient/PatientModuleMsApplicationTests.java
+++ back-end/patientModuleMS/src/test/java/com/medicalservicesystem/patient/PatientModuleMsApplicationTests.java
@@ -1,12 +1,9 @@
 package com.medicalservicesystem.patient;
 
-<<<<<<< HEAD
-//import static org.junit.Assert.assertEquals;
-=======
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
->>>>>>> origin/komali
+
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -80,129 +77,5 @@ class PatientModuleMsApplicationTests {
 		blogServiceImpl.updateBlog(blog3);
 		assertEquals(blog, blogServiceImpl.getBlog(1L));	
 	}*/
-<<<<<<< HEAD
-=======
-//	@InjectMocks
-//	private PatientServiceImp patientServiceImp;
-//
-//	@Mock
-//	private PatientRepository patientRepository;
-//	
-//
-//	@Test
-//	public void  addPatientTest() {
-//		Patient patient =new Patient();
-//		when(patientRepository.save(patient)).thenReturn(new Patient());
-//		assertEquals(patient, patientServiceImp.addPatient(patient));
-//	}
-//	
-//	@Test
-//	public void deleteBlogById() {
-//		Blog blog2 = new Blog(1L,"test3");
-//		blogRepository.deleteById(blog2.getId());
-//		assertEquals(null, blogServiceImpl.getBlog(1L));	
-//	}
-//	
-//
-<<<<<<< HEAD
-//	public Patient findPatientById(Long id);
-//
-//	public void deletePatientById(Long id);
 
-/*	@Test
-	public void  addPatientTest() {
-		Patient patient =new Patient();
-		when(patientRepository.save(patient)).thenReturn(new Patient());
-		assertEquals(patient, patientServiceImp.addPatient(patient));
-	}
-	
-	@Test
-	public void deleteBlogById() {
-		Blog blog2 = new Blog(1L,"test3");
-		blogRepository.deleteById(blog2.getId());
-		assertEquals(null, blogServiceImpl.getBlog(1L));	
-	}
-	
-
-	
-	@Test
-	public void findPatientByIdTest() {
-		Blog blog = new Blog(1L,"test1");
-		Blog blog1 = new Blog(2L,"test2");
-		Blog blog2 = new Blog(3L,"test3");
-		Blog blog3 = new Blog(4L,"test4");
-		when(blogRepository.findById(blog.getId())).thenReturn(Optional.of(blog));
-		when(blogRepository.findById(blog1.getId())).thenReturn(Optional.of(blog1));
-		when(blogRepository.findById(blog2.getId())).thenReturn(Optional.of(blog2));
-		when(blogRepository.findById(blog3.getId())).thenReturn(Optional.of(blog3));
-		assertEquals(blog1, blogServiceImpl.getBlog((long) 2));	
-	}
-	@Test
-	public void getPatientTest() {
-		Blog blog = new Blog(1L,"test1");
-		Blog blog1 = new Blog(2L,"test2");
-		Blog blog2 = new Blog(3L,"test3");
-		Blog blog3 = new Blog(4L,"test4");
-		when(blogRepository.findById(blog.getId())).thenReturn(Optional.of(blog));
-		when(blogRepository.findById(blog1.getId())).thenReturn(Optional.of(blog1));
-		when(blogRepository.findById(blog2.getId())).thenReturn(Optional.of(blog2));
-		when(blogRepository.findById(blog3.getId())).thenReturn(Optional.of(blog3));
-		assertEquals(blog1, blogServiceImpl.getBlog((long) 2));	
-	}
-	@Test
-	public void updatePatientTest() {
-		Blog blog = new Blog(1L,"test1");
-		Blog blog1 = new Blog(2L,"test2");
-		Blog blog2 = new Blog(3L,"test3");
-		Blog blog3 = new Blog(1L,"test4");
-		when(blogRepository.findById(blog.getId())).thenReturn(Optional.of(blog));
-		when(blogRepository.findById(blog1.getId())).thenReturn(Optional.of(blog1));
-		when(blogRepository.findById(blog2.getId())).thenReturn(Optional.of(blog2));
-		blogServiceImpl.updateBlog(blog3);
-		assertEquals(blog, blogServiceImpl.getBlog(1L));	
-	}
-*/
-=======
-//	
-//	@Test
-//	public void findPatientByIdTest() {
-//		Blog blog = new Blog(1L,"test1");
-//		Blog blog1 = new Blog(2L,"test2");
-//		Blog blog2 = new Blog(3L,"test3");
-//		Blog blog3 = new Blog(4L,"test4");
-//		when(blogRepository.findById(blog.getId())).thenReturn(Optional.of(blog));
-//		when(blogRepository.findById(blog1.getId())).thenReturn(Optional.of(blog1));
-//		when(blogRepository.findById(blog2.getId())).thenReturn(Optional.of(blog2));
-//		when(blogRepository.findById(blog3.getId())).thenReturn(Optional.of(blog3));
-//		assertEquals(blog1, blogServiceImpl.getBlog((long) 2));	
-//	}
-//	@Test
-//	public void getPatientTest() {
-//		Blog blog = new Blog(1L,"test1");
-//		Blog blog1 = new Blog(2L,"test2");
-//		Blog blog2 = new Blog(3L,"test3");
-//		Blog blog3 = new Blog(4L,"test4");
-//		when(blogRepository.findById(blog.getId())).thenReturn(Optional.of(blog));
-//		when(blogRepository.findById(blog1.getId())).thenReturn(Optional.of(blog1));
-//		when(blogRepository.findById(blog2.getId())).thenReturn(Optional.of(blog2));
-//		when(blogRepository.findById(blog3.getId())).thenReturn(Optional.of(blog3));
-//		assertEquals(blog1, blogServiceImpl.getBlog((long) 2));	
-//	}
-//	@Test
-//	public void updatePatientTest() {
-//		Blog blog = new Blog(1L,"test1");
-//		Blog blog1 = new Blog(2L,"test2");
-//		Blog blog2 = new Blog(3L,"test3");
-//		Blog blog3 = new Blog(1L,"test4");
-//		when(blogRepository.findById(blog.getId())).thenReturn(Optional.of(blog));
-//		when(blogRepository.findById(blog1.getId())).thenReturn(Optional.of(blog1));
-//		when(blogRepository.findById(blog2.getId())).thenReturn(Optional.of(blog2));
-//		blogServiceImpl.updateBlog(blog3);
-//		assertEquals(blog, blogServiceImpl.getBlog(1L));	
-//	}
->>>>>>> origin/sourav
-=======
->>>>>>> origin/sourav
-
->>>>>>> origin/sourav
 }
