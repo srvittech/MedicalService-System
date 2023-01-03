b7b8b1e82436515d727f19cd0e8ca76746a9a578
Sourav Kumar <srvkrak@gmail.com>
2023-01-03 14:02:38 +0530

    angular material added front ui design in progress

--- front-end/Medical_Service_System/src/app/app.module.ts
+++ front-end/Medical_Service_System/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorComponentComponent } from './model/doctor-component/doctor-component.component';
+import { PatientComponentComponent } from './model/patient-component/patient-component.component';
+import { KioskComponentComponent } from './model/kiosk-component/kiosk-component.component';
+import { AdminComponentComponent } from './model/admin-component/admin-component.component';
+import { LoginComponentComponent } from './model/login-component/login-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorComponentComponent,
+    PatientComponentComponent,
+    KioskComponentComponent,
+    AdminComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/Medical_Service_System/src/app/model/doctor-component/doctor-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-component',
+  templateUrl: './doctor-component.component.html',
+  styleUrls: ['./doctor-component.component.css']
+})
+export class DoctorComponentComponent {
+
+}
