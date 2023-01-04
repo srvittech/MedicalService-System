6104b595a1c3f2002b48294a976c13abaa654ab9
Sourav Kumar <srvkrak@gmail.com>
2023-01-04 14:54:15 +0530

    ok

--- front-end/Medical_Service_System/src/app/app.component.ts
+++ front-end/Medical_Service_System/src/app/app.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Medical_Service_System';
- 
+  colorLight: any = "#3E6D9C"
+  colorDark: any = "#001253"
+  colorDiary: any = {
+    about: this.colorDark,
+    doctor: this.colorLight,
+    patient: this.colorLight,
+    kiosk: this.colorLight,
+    admin: this.colorLight
+  }
+
+  changeColor(component: any) {
+    for (let i in this.colorDiary) {
+      if (component == i) {
+        this.colorDiary[i] = this.colorDark
+      }
+      else {
+        this.colorDiary[i] = this.colorLight
+      }
+    }
+  }
+
 }
