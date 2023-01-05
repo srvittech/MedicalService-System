6664614b2dafcfc51f151f164ce18e1ecadfa054
Komali.Indurthi <Komali.Indurthi@thryvedigital.com>
2023-01-05 15:47:56 +0530

    ok

--- front-end/Medical_Service_System/src/app/model/admin-component/add-doctor-dialog/add-doctor-dialog.component.ts
+++ front-end/Medical_Service_System/src/app/model/admin-component/add-doctor-dialog/add-doctor-dialog.component.ts
@@ -16,12 +16,14 @@ export class AddDoctorDialogComponent {
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
       name:['',Validators.required],
+      specialization:['',Validators.required],
       email:['', [Validators.required,Validators.email]],
       password:['', [Validators.required,Validators.minLength(6)]],
-      mobile:['', [Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]]
+      mobile:['', [Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      yearsOfExperience:['',Validators.required]
     })
-    
   }
+  roles:any= ["Eye", "ENT", "Heart"]
     signup(){
       this.submitted = true
     if (this.signupForm.invalid){return}
