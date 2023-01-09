20dabb117904201eeaaa4258c9268a7409576c8d
Komali.Indurthi <Komali.Indurthi@thryvedigital.com>
2023-01-09 20:35:35 +0530

    ok

--- back-end/doctormodule/src/main/java/com/medicalservice/doctor/DoctormoduleApplication.java
+++ back-end/doctormodule/src/main/java/com/medicalservice/doctor/DoctormoduleApplication.java
@@ -3,10 +3,9 @@ package com.medicalservice.doctor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class DoctormoduleApplication {
 
 	public static void main(String[] args) {
