2f670cc774ed6357e4f8d6115361f37162f3a524
Sourav Kumar <srvkrak@gmail.com>
2023-01-10 12:26:26 +0530

    okay

--- front-end/Medical_Service_System/src/app/model/patient-component/patient-feedback/patient-feedback.component.ts
+++ front-end/Medical_Service_System/src/app/model/patient-component/patient-feedback/patient-feedback.component.ts
@@ -1,8 +1,9 @@
-import { Component,OnInit } from '@angular/core';
-import { FormGroup, Validators ,FormBuilder} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { FeedbackServiceService } from 'src/app/services/feedback-Service/feedback-service.service';
 import { TransactionServiceService } from 'src/app/services/transaction-Service/transaction-service.service';
+import { PopUpComponent } from '../../pop-up/pop-up.component';
 
 @Component({
   selector: 'app-patient-feedback',
@@ -10,32 +11,72 @@ import { TransactionServiceService } from 'src/app/services/transaction-Service/
   styleUrls: ['./patient-feedback.component.css']
 })
 export class PatientFeedbackComponent implements OnInit {
-  public feedbackForm !: FormGroup;
-  myCurrentTransactionId:any = ""
-  submitted =false
-  constructor(private dialog:MatDialog,private transactionService:TransactionServiceService,private formBuilder: FormBuilder){}
-  ngOnInit(): void {
-    this.feedbackForm = this.formBuilder.group({
-      transactionId:[this.transactionService.myCurrentTransactionId],
-      feedback: ['', Validators.required]
-  })
+  m1: any = ""
+  m2: any = ""
+  flag: boolean = false
 
-}
-submit(){
-  this.submitted=true
-  console.log("hi");
-  
-  console.table(this.feedbackForm.value);
-  
-  if (!this.feedbackForm.invalid){
-    this.transactionService.updateTransaction(this.feedbackForm.value).subscribe(res=>{
-      alert("Feedback Added Successfully")
-      this.dialog.closeAll()
+  openDialog(): void {
+    const dialogRef = this.dialog.open(PopUpComponent, {
+      data: { m1: this.m1, m2: this.m2 },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+    })
+  }
+  openDialogLogout(): void {
+    const dialogRef = this.dialog.open(PopUpComponent, {
+      data: {m1:"SuccesFully LoggedOut"},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      
     })
   }
 
-}
-close(){
-  this.dialog.closeAll()
-}
+  public feedbackForm !: FormGroup;
+  myCurrentTransactionId: any = ""
+  submitted = false
+  constructor(private dialog: MatDialog, private transactionService: TransactionServiceService, private formBuilder: FormBuilder) { }
+  ngOnInit(): void {
+    this.feedbackForm = this.formBuilder.group({
+      transactionId: [this.transactionService.myCurrentTransactionId],
+      feedback: ['', Validators.required]
+    })
+
+  }
+  submit() {
+    this.submitted = true
+    console.log("hi");
+
+    console.table(this.feedbackForm.value);
+
+    if (!this.feedbackForm.invalid) {
+      this.transactionService.updateTransaction(this.feedbackForm.value).subscribe(res => {
+        this.flag = true
+        this.dialog.closeAll()
+      })
+    }
+    setTimeout(() => {
+      if (this.flag) {
+        this.m1 = "Feedback Added Successfully"
+        this.m2 = "Thanks"
+        this.dialog.closeAll()
+        console.log("true");
+
+      }
+      else {
+        this.m1 = "Something Went Wrong"
+        this.m2 = "Try Again"
+        console.log("false");
+
+      }                       // <<<---using ()=> syntax
+      this.openDialog()
+    }, 2000);
+
+  }
+  close() {
+    this.dialog.closeAll()
+    this.openDialogLogout()
+  }
 }
